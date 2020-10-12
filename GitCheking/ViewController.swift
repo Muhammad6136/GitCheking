f7b1349c77a155c08f99bb8fd7cfad3bd4e5837e
Muhammad <hussainmuhammad603@gmail.com>
2020-10-12 20:43:00 +0500

    Added Label + Print In VC

--- GitCheking/ViewController.swift
+++ GitCheking/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("Nothing to write freec ode please tell me where is the ball of epic senario")
     }
 
 
